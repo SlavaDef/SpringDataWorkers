b48a2093e83bfdf98dd98c617305adcc04df75ed
SlavaDef <slavadef@gmail.com>
2023-11-27 22:14:15 +0200

    First commit

---
+++ src/main/java/com/task/dataspringworkers/DataSpringWorkersApplication.java
@@ -0,0 +1,13 @@
+package com.task.dataspringworkers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataSpringWorkersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataSpringWorkersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/task/dataspringworkers/DataSpringWorkersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.dataspringworkers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataSpringWorkersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
