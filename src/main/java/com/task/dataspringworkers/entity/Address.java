e3c05e4b68f2962a48d3c3fc647f36e49abcf47e
SlavaDef <slavadef@gmail.com>
2023-11-27 22:18:31 +0200

    new 1

--- src/main/java/com/task/dataspringworkers/entity/Address.java
+++ src/main/java/com/task/dataspringworkers/entity/Address.java
@@ -1,4 +1,11 @@
 package com.task.dataspringworkers.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Address {
+
+    @Id
+    private Long adressId;
 }
